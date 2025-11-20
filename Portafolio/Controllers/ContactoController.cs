a6a6573840e49e62b09c0d9cda00dad1ecc766d5
AngelSanchezMedina <angelobedsm@gmail.com>
2025-11-19 23:09:04 -0700

    Primer commit limpio

---
+++ Portafolio/Controllers/ContactoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Servicios;
+
+namespace Portafolio.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly IRepositorioContactos repositorioContactos;
+
+        public ContactoController(IRepositorioContactos repositorioContactos)
+        {
+            this.repositorioContactos = repositorioContactos;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(ContactoViewModel contactoViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(contactoViewModel);
+            }
+
+            await repositorioContactos.Crear(contactoViewModel);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Portafolio/Servicios/RepositorioContactos.cs
@@ -0,0 +1,31 @@
+﻿using Portafolio.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioContactos
+    {
+        Task Crear(ContactoViewModel contactoViewModel);
+    }
+    public class RepositorioContactos : IRepositorioContactos
+    {
+
+        private readonly string connectionString;
+
+        public RepositorioContactos(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(ContactoViewModel contactoViewModel)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(
+                @"INSERT INTO Contacto (nombre, email, mensaje) 
+            VALUES( @nombre, @email, @mensaje);
+
+            SELECT SCOPE_IDENTITY();", contactoViewModel);
+        }
+    }
+}
